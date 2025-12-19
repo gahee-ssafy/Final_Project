ce4b0a28a4ed20f50a42b808369aac6f72ed9f58
전가희 <krjgh@naver.com>
2025-12-20 08:31:56 +0900

    whiteboard

--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('deposit/', views.deposit_products), # 예적금 조회
-    path('spot/', views.spot_price),          # 현물(금/은) 조회
-]
--- project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # products 앱의 URL을 /api/v1/products/ 로 시작하게 연결
-    path('api/v1/products/', include('products.urls')),
-]
